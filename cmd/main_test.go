43bed3e9d55f62d1910d298a6850729b9209aeaf
kimura-hytaae <92291361+kimura-hytaae@users.noreply.github.com>
2023-04-30 15:30:11 +0900

    Update main_test.go

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -2,14 +2,15 @@ package main
 
 import "testing"
 
-func Example_Main() {
-	goMain([]string{})
-	// Output:
-	// Hello World
+func Example_main() {
+  goMain([]string{})
+  // Output:
+  // Hello World
 }
 
 func Test_Main(t *testing.T) {
-	if status := goMain([]string{}); status != 0 {
-		t.Error("Expected 0, got ", status)
-	}
+  if status := goMain([]string{});
+  status != 0 { 
+    t.Error("Expected 0, got ", status)
+  }
 }
